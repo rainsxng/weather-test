462087e87588c52f90fb0bf7511a0609316aa7df
Dmitriy Bohdanov <rainsxng@gmail.com>
2021-04-06 19:49:25 +0300

    chore: initial commit

---
+++ weather-api/index.ts
@@ -0,0 +1,12 @@
+import * as express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello world');
+});
+
+app.listen(3001, () => {
+    console.log('works');
+});
+
